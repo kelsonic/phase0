faddad169e8ca7c5278a89e658e2551ec76134f1
kgadams93 <kgadams93@gmail.com>
2016-03-01 16:10:28 +0000

    Complete 4.5.2

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,22 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def get_grade(average)
+  grade = average.to_i
+  if grade >= 90
+    p 'A'
+  elsif grade >= 80
+    p 'B'
+  elsif grade >= 70
+    p 'C'
+  elsif grade >= 60
+    p 'D'
+  else
+    p 'F'
+  end
+  
+end

288665be297585bec2a31b6d65b438bd02ae8e09
kgadams93 <kgadams93@gmail.com>
2016-03-01 12:32:11 +0000

    Complete 4.3.2 math ruby challenge

--- week-4/define-method/my_solution.rb
+++ week-4/define-method/my_solution.rb
@@ -1,7 +1,7 @@
 # Define an Empty Method
 def do_something(param1, param2, param3)
 end
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Your Solution Below

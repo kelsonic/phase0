bbd9977d97f39e2aea3a3fab5c2ff58ea678e15b
kgadams93 <kgadams93@gmail.com>
2016-03-22 18:21:32 +0000

    Complete 7.3

---
+++ week-7/eloquent.js
@@ -0,0 +1,117 @@
+// Eloquent JavaScript
+
+// Run this file in your terminal using `node my_solution.js`. Make sure it works before moving on!
+
+// Introduction
+// Did you learn anything new about JavaScript or programming in general?
+
+// Answer
+
+// Are there any concepts you feel uncomfortable with?
+
+// Answer
+
+// Ch. 1: Values, Types, and Operators
+// Identify two similarities and two differences between JavaScript and Ruby syntax with regard to numbers, arithmetic, strings, booleans, and various operators.
+
+// Answer
+
+// Ch. 2: Program Structure
+// What is an expression?
+
+// Answer
+
+// What is the purpose of semicolons in JavaScript? Are they always required?
+
+// Answer
+
+// What causes a variable to return undefined?
+
+// Answer
+
+// Write your own variable and do something to it in the eloquent.js file.
+
+// Answer
+
+// What does console.log do and when would you use it? What Ruby method(s) is this similar to?
+
+// Answer
+
+// Write a short program that asks for a user to input their favorite food. 
+// After they hit return, have the program respond with "Hey! That's my favorite 
+// too!" (You will probably need to run this in the Chrome console rather than 
+// node since node does not support prompt or alert). Paste your program into 
+// the eloquent.js file.
+
+// Answer
+
+// Describe while and for loops
+
+// Answer
+
+// What other similarities or differences between Ruby and JavaScript did you 
+// notice in this section?
+
+// Answer
+
+// Complete at least one of the exercises (Looping a Triangle, FizzBuzz, of 
+// Chess Board) in the eloquent.js file.
+
+// Answer
+
+// Ch. 3: Functions
+// What are the differences between local and global variables in JavaScript?
+
+// Answer
+
+// When should you use functions?
+
+// Answer
+
+// What is a function declaration?
+
+// Answer
+
+// Complete the minimum exercise in the eloquent.js file.
+
+// Answer
+
+// Ch. 4: Data Structures: Objects and Arrays
+// Skip the sections on the Lycanthrope's log, Computing Correlations, and 
+// sections from Further Arrayology to the Global Object, but read the chapter summary.
+
+// Answer
+
+// What is the difference between using a dot and a bracket to look up a property? 
+// Ex. array.max vs array["max"]
+
+// Answer
+
+// Create an object called me that stores your name, age, three favorite foods, 
+// and a quirk in your eloquent.js file.
+
+// Answer
+
+// What is a JavaScript object with a name and value property similar to in Ruby?
+
+// Answer
+
+
+// Program Structure
+// Write your own variable and do something to it.
+
+
+
+
+// Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
+
+
+
+// Functions
+
+// Complete the `minimum` exercise.
+
+
+
+// Data Structures: Objects and Arrays
+// Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
---
+++ week-7/my_solution.rb
@@ -0,0 +1,96 @@
+# Virus Predictor
+
+# I worked on this challenge [by myself, with: ].
+# We spent [#] hours on this challenge.
+
+# EXPLANATION OF require_relative
+# Require relative is used with files within the same directory whereas require is used with external files or files that need to have an absolute path.
+#
+require_relative 'state_data'
+
+class VirusPredictor
+
+  # Initialize (.new), takes local variables and stores them in instance variables
+  def initialize(state_of_origin, population_density, population)
+    @state = state_of_origin
+    @population = population
+    @population_density = population_density
+  end
+
+  # Calls the predicted_deaths and speed_of_spread instance methods
+  def virus_effects
+    predicted_deaths(@population_density, @population, @state)
+    speed_of_spread(@population_density, @state)
+  end
+
+  private
+
+  # Takes local variables as parameters and calculates number_of_deaths from population density instance variable
+  def predicted_deaths(population_density, population, state)
+    # predicted deaths is solely based on population density
+    if @population_density >= 200
+      number_of_deaths = (@population * 0.4).floor
+    elsif @population_density >= 150
+      number_of_deaths = (@population * 0.3).floor
+    elsif @population_density >= 100
+      number_of_deaths = (@population * 0.2).floor
+    elsif @population_density >= 50
+      number_of_deaths = (@population * 0.1).floor
+    else
+      number_of_deaths = (@population * 0.05).floor
+    end
+
+    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+
+  end
+
+  # Takes local variables as parameters, initializes speed float at 0, increases speed depending on population density
+  def speed_of_spread(population_density, state) #in months
+    # We are still perfecting our formula here. The speed is also affected
+    # by additional factors we haven't added into this functionality.
+    speed = 0.0
+
+    if @population_density >= 200
+      speed += 0.5
+    elsif @population_density >= 150
+      speed += 1
+    elsif @population_density >= 100
+      speed += 1.5
+    elsif @population_density >= 50
+      speed += 2
+    else
+      speed += 2.5
+    end
+
+    puts " and will spread across the state in #{speed} months.\n\n"
+
+  end
+
+end
+
+#=======================================================================
+
+# DRIVER CODE
+ # initialize VirusPredictor for each state
+
+STATE_DATA.each do |key, value|
+  state = VirusPredictor.new(key, STATE_DATA[key][:population_density], STATE_DATA[key][:population])
+  state.virus_effects
+end
+
+
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
+
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
+
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
+
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
+
+
+#=======================================================================
+# Reflection Section

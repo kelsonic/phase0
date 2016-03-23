84039061089e411608b678c1e1c25e46c797c653
kgadams93 <kgadams93@gmail.com>
2016-03-23 14:20:55 +0000

    Complete 7.6, need to add reflection

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,77 @@
+ // JavaScript Olympics
+
+// I paired with Leland on this challenge.
+
+// This challenge took me [1.5] hours.
+
+
+// Warm Up
+
+
+
+
+// Bulk Up
+//  var addWin = function(athletes) {
+//  	for (i in athletes){
+//  		athletes[i] = athletes[i].name +" won the "+ athletes[i].event + "!";
+//  		console.log(athletes[i]);
+//  	};
+//  	console.log(athletes);
+//  };
+
+// var sarah = {
+// 	name: "Sarah Hughes",
+// 	event: "Ladies' Singles"
+// };
+
+// addWin([sarah]);
+
+
+// Jumble your words
+
+// var jumbler = function(myString) {
+// 	console.log(myString.split("").reverse().join(""))
+// 	return myString.split("").reverse().join("")
+// }
+
+// var myString = "Jumble This"
+
+// jumbler(myString);
+
+// 2,4,6,8
+
+	// function evens(numbers){
+	// 	for (var i = 0; i < numbers.length; i++){
+	// 		if (numbers[i] % 2 == 0){
+	// 			console.log(numbers[i])
+				
+	// 		};
+	// 	}
+	// }
+	// var numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+	// evens(numbers)
+			
+
+
+// "We built this city"
+
+// function Athlete(name, age, sport, quote){
+// 	this.name = name;
+// 	this.age = age;
+// 	this.sport = sport;
+// 	this.quote = quote;
+
+// }
+
+
+// var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+// console.log(michaelPhelps.constructor === Athlete)
+// console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+
+
+
+
+
+
+// Reflection
